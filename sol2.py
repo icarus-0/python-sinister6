927f28abca04765f276d72349045a2f0884b5b71
sinister6 <naman.chandel_cs17@gla.ac.in>
2019-10-13 23:43:51 +0530

    Committed

---
+++ sol2.py
@@ -0,0 +1,19 @@
+def fund(l,k):
+    x=[]
+    for i in range(len(l)):
+        su=0
+
+        for j in range(i,len(l)):
+            su+=l[j]
+            if su<=k:
+                x.append(l[j])
+            else:
+                break
+        if sum(x)==k:
+            break
+        else:
+            x=[]
+    if len(x)>0:
+        return x
+    else:
+        return None
